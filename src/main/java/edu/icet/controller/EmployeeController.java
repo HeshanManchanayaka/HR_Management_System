ef696e65ecd8124dbe4d766cbd5ec43aa9d6905d
HeshanManchanayaka <hdmanchanayaka@gmail.com>
2025-04-29 10:58:38 +0530

    update employee Controller

--- src/main/java/edu/icet/controller/EmployeeController.java
+++ src/main/java/edu/icet/controller/EmployeeController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@RestController
+@RequestMapping("/employee")
+@CrossOrigin
 @RequiredArgsConstructor
 public class EmployeeController {
 
