e1b0d4b62aa444dc11ab879a2d7a0059e2ab3308
HeshanManchanayaka <hdmanchanayaka@gmail.com>
2025-04-29 10:47:55 +0530

    update employee dto

--- src/main/java/edu/icet/dto/Employee.java
+++ src/main/java/edu/icet/dto/Employee.java
@@ -4,9 +4,17 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Employee {
     private Integer id;
 
