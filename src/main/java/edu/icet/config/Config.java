a4b5ab99a43ff3e2c5a9e02de2a241329f481c57
HeshanManchanayaka <hdmanchanayaka@gmail.com>
2025-04-29 12:04:35 +0530

    update config file with date and time

--- src/main/java/edu/icet/config/Config.java
+++ src/main/java/edu/icet/config/Config.java
@@ -1,5 +1,7 @@
 package edu.icet.config;
 
+import edu.icet.dto.Employee;
+import edu.icet.entity.EmployeeEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,10 @@ import org.springframework.context.annotation.Configuration;
 public class Config {
     @Bean
     public ModelMapper getMapper(){
-        return  new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+        mapper.typeMap(EmployeeEntity.class, Employee.class)
+                .addMapping(EmployeeEntity::getCreatedAt, Employee::setCreateAt)
+                .addMapping(EmployeeEntity::getUpdatedAt, Employee::setUpdateAt);
+        return mapper;
     }
 }
