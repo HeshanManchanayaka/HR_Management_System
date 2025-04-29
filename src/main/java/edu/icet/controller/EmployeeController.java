87eaa32da7315bee3090f05b68bd5c5111c779bd
HeshanManchanayaka <hdmanchanayaka@gmail.com>
2025-04-29 10:56:36 +0530

    create EmployeeController

---
+++ src/main/java/edu/icet/controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package edu.icet.controller;
+
+import edu.icet.dto.Employee;
+import edu.icet.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    final EmployeeService service;
+
+    @PostMapping("/add")
+    public void addEmployee(@RequestBody Employee employee){
+        service.addEmployee(employee);
+    }
+
+    @GetMapping("/getAll")
+    public List<Employee> getAll(){
+       return service.getAll();
+    }
+
+    @PutMapping("/update")
+    public void updateEmployee(@RequestBody Employee employee){
+        service.updateEmployee(employee);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public Employee searchById(@PathVariable Integer id){
+        return  service.searchById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteEmployee(@PathVariable Integer  id){
+        service.deleteEmployee(id);
+    }
+
+
+}
